c7c3d5a79971e2956583409cf6c0d7a378f0a65d
yusekim <youwin0802@naver.com>
2024-11-03 18:07:52 +0900

    fix: docker compose

--- BE/start.sh
+++ BE/start.sh
@@ -1,4 +1,6 @@
 python manage.py makemigrations pong
-python manage.py migrate 
+python manage.py migrate
 
 exec python manage.py runsslserver 0.0.0.0:443 --certificate /app/django.crt --key /app/django.key
+
+
---
+++ ganache/start.sh
@@ -0,0 +1,1 @@
+exec ganache-cli -d --host 0.0.0.0 --port 8545 -e 10000000000
